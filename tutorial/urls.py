8bba629dc6cd7678daa63c0bee8c6b12d550c10f
tfarrell-vz <farrell@abiresearch.com>
2014-10-06 14:09:46 -0400

    Wire up the snippet app's URLs

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tutorial.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include('snippets.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
