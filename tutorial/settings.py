5903f77f4a347d5998e57da2acf546efa75fd0a3
tfarrell-vz <farrell@abiresearch.com>
2014-10-06 14:08:16 -0400

    Install the framework and the app. Set the database to an sqlite3 file.

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'snippets',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -59,7 +62,11 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'tmp.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
