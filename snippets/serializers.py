1d8826fdf7c6d24be3a4a2602a709108b7b22ae9
tfarrell-vz <farrell@abiresearch.com>
2014-10-06 15:31:22 -0400

    Serializer functioning

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -7,7 +7,7 @@ class SnippetSerializer(serializers.Serializer):
     title = serializers.CharField(required=False, max_length = 100)
     code = serializers.CharField(widget=widgets.Textarea, max_length=100000)
     linenos = serializers.BooleanField(required=False)
-    language = serializers.Choicefield(choices=LANGUAGE_CHOICES,
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES,
                                       default='python')
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
 
